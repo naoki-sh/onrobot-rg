7aad8be29c5389f18d873c8ac5e68f4539730f42
Naoki Shirakura <shirakura.naoki@aist.go.jp>
2023-06-06 17:44:28 +0900

    modify scripts for mg10 gripper

--- mg10/src/demo.py
+++ mg10/src/demo.py
@@ -1,46 +1,40 @@
 #!/usr/bin/env python3
 
-import time
 import argparse
 
-from onrobot import RG
-
+from onrobot import MG
+import time
 
 def run_demo():
-    """Runs gripper open-close demonstration once."""
-    rg = RG(gripper, toolchanger_ip, toolchanger_port)
+    """interactive controller demo."""
+    mg = MG(toolchanger_ip, toolchanger_port)
+    while True:
+        mg.get_status()
+        print(f"Target strength: {mg.get_current_strength()} %")
+        print("Command list:")
+        print("  s: set strength, g: grip, r: release, q: quit")
+        c = input("command > ")
+        if c == "s":
+            c = input("Target strength (0 ~ 100) > ")
+            target_strength = int(c)
+            if 0 <= target_strength <= 100:
+                mg.set_target_strength(target_strength)
+            else:
+                print("Target strength must be 0 ~ 100")
+        elif c == "g":
+            mg.set_command(1)
+        elif c == "r":
+            mg.set_command(0)
+        elif c == "q":
+            break
 
-    if not rg.get_status()[0]:  # not busy
-        print("Current hand opening width: " +
-              str(rg.get_width_with_offset()) +
-              " mm")
-
-        rg.open_gripper()     # fully opened
-        while True:
-            time.sleep(0.5)
-            if not rg.get_status()[0]:
-                break
-        rg.close_gripper()    # fully closed
-        while True:
-            time.sleep(0.5)
-            if not rg.get_status()[0]:
-                break
-        rg.move_gripper(800)  # move to middle point
-        while True:
-            time.sleep(0.5)
-            if not rg.get_status()[0]:
-                break
-
-    rg.close_connection()
-
+    mg.set_command(0)
+    mg.close_connection()
+    exit(0)
 
 def get_options():
     """Returns user-specific options."""
     parser = argparse.ArgumentParser(description='Set options.')
-    parser.add_argument(
-        '--gripper', dest='gripper', type=str,
-        default="rg6", choices=['rg2', 'rg6'],
-        help='set gripper type, rg2 or rg6')
     parser.add_argument(
         '--ip', dest='ip', type=str, default="192.168.1.1",
         help='set ip address')
@@ -52,7 +46,6 @@ def get_options():
 
 if __name__ == '__main__':
     args = get_options()
-    gripper = args.gripper
     toolchanger_ip = args.ip
     toolchanger_port = args.port
     run_demo()
